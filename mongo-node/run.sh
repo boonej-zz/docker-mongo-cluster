0896495bfe884bbd5fbd0da7ebb1b17391199312
Jonathan Boone <jb@looklive.com>
2017-02-09 16:01:58 -0600

    added config servers and router

--- mongo-node/run.sh
+++ mongo-node/run.sh
@@ -3,6 +3,7 @@
 MONGOD=/usr/bin/mongod
 MONGO=/usr/bin/mongo
 LOG_PATH=/var/log/mongod/mongod.log
+PORT=27017
 
 check_status()
 {
@@ -16,23 +17,70 @@ check_status()
   done
 }
 
+configure_replication ()
+{
+  $MONGO --host 127.0.0.1:${PORT} --eval 'rs.initiate()'
+  check_status ${S1}:${PORT}
+  $MONGO --host 127.0.0.1:${PORT} --eval "rs.add(\"${S1}:${PORT}\")"
+  check_status $S2:${PORT}
+  $MONGO --host 127.0.0.1:${PORT} --eval "rs.add(\"${S2}:${PORT}\")"
+}
+
 build_replica_set ()
 {
-  $MONGOD --fork --replSet $RS_NAME --logpath \
+  PORT=27018
+  mv /data/restore /data/db
+  $MONGOD --fork --shardsvr --replSet $RS_NAME --logpath \
     $LOG_PATH 
   if [ $STATE = 'primary' ]
   then
-    $MONGO --eval 'rs.initiate()'
-    check_status $S1
-    $MONGO --eval "rs.add(\"${S1}\")"
-    check_status $S2
-    $MONGO --eval "rs.add(\"${S2}\")"
+    configure_replication
   fi
+}
+
+build_config_set ()
+{
+  PORT=27019
+  rm -r /data/restore
+  mkdir /data/configdb
+  $MONGOD --fork --replSet $RS_NAME --configsvr \
+    --logpath $LOG_PATH
+  if [ $STATE = 'primary' ]
+  then
+    configure_replication
+  fi
+}
+
+build_router ()
+{
+  PORT=27017
+  LOG_PATH=/var/log/mongos.log
+  check_status mdb-cs1:27019
+  check_status mdb-cs2:27019
+  check_status mdb-cs3:27019
+  /usr/bin/mongos --configdb cs/mdb-cs1,mdb-cs2,mdb-cs3 \
+    --logpath $LOG_PATH --fork
+  SHARD1=rs1/mdb-s1r1:27018,mdb-s1r2:27018,mdb-s1r3:27018
+  SHARD2=rs2/mdb-s2r1:27018,mdb-s2r2:27018,mdb-s2r3:27018
+  check_status $SHARD1
+  $MONGO --eval "sh.addShard(\"${SHARD1}\")"
+  check_status $SHARD2
+  $MONGO --eval "sh.addShard(\"${SHARD2}\")"
 }
 
 if [ $SHARD_ENV = 'replicaset' ]
 then
   build_replica_set
+fi
+
+if [ $SHARD_ENV = 'configset' ]
+then
+  build_config_set
+fi
+
+if [ $SHARD_ENV = 'router' ]
+then
+  build_router
 fi
 
 tailf $LOG_PATH
